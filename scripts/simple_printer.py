e6b70365d1ca1132d17d46358060e6c89ae2021e
thvs86 <mertealaur@gmail.com>
2020-06-08 22:42:28 +0200

    Develop simple printer

---
+++ scripts/simple_printer.py
@@ -0,0 +1,16 @@
+# Objectives
+## To begin with, you should develop a simple printer. Your program must show the text from the output example.
+
+# Example
+## Output:
+# I do love animals!
+# Start looking after animals...
+# Deer looks fine.
+# Bat looks happy.
+# Lion looks healthy.
+
+print("I do love animals!")
+print("Start looking after animals...")
+print("Deer looks fine.")
+print("Bat looks happy.")
+print("Lion looks healthy.")
