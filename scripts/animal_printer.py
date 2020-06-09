89ef57cd1e5e4fe69e0a72d2cffbb307b76730d6
thvs86 <mertealaur@gmail.com>
2020-06-10 00:18:03 +0200

    Formatting

--- scripts/animal_printer.py
+++ scripts/animal_printer.py
@@ -3,6 +3,7 @@
 # We need to see the animals on the screen to know how they are doing,
 # right? At this time, we are ready to print something awesome: animal images!
 
+
 # Objectives
 # In the second stage, you need to develop an animal printer.
 # Your program should show a certain animal: 
@@ -11,6 +12,7 @@
 # Please, don't remove the r character at the start of the code template.
 # It's a part of the string and it's important.
 # So, the string should start with r""" sequence.
+
 
 # Example
 # Your output should contain the following ASCII image:
@@ -33,6 +35,7 @@
 #           ;,,      !,!   !,!     ;,;
 #           /_I      L_I   L_I     /_I
 # Yey, our little camel is sunbathing!
+
 
 print(r"""
 Switching on camera from habitat with camels...
